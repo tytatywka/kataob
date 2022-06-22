cad66c5a626ef522d1dce86a8d377f578274f997
tytatywka@gmail.com <tytatywka@gmail.com>
2022-06-22 19:56:14 +0300

    Initial commit

---
+++ src/Kata/Main.java
@@ -0,0 +1,11 @@
+package Kata;
+
+import java.io.*;
+import java.util.*;
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+    }
+}
+
---
+++ src/Kata/Sendable.java
@@ -0,0 +1,4 @@
+package Kata;
+
+public interface Sendable<T> {
+}
